d0598b4839e4d2a9d2eb4bb3e733ecc95dd2c617
SMhd <smhd3000@gmail.com>
2024-09-10 11:41:10 +0330

    feat: increase staleTime for queries to 1 hour

--- lib/queryClient.ts
+++ lib/queryClient.ts
@@ -3,7 +3,7 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
-			staleTime: 2 * 60 * 1000,
+			staleTime: 1 * 60 * 60 * 1000,
 			gcTime: Number.POSITIVE_INFINITY,
 			retry: false,
 		},
