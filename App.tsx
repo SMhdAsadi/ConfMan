f6e6395d5ba23831cbd32a114f690506335ff727
SMhd <smhd3000@gmail.com>
2024-09-10 10:07:05 +0330

    feat: show SplashScreen until Auth state is loaded

--- App.tsx
+++ App.tsx
@@ -1,19 +1,33 @@
 import "./lib/unistyles";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
 import { PaperProvider } from "react-native-paper";
-import { AuthProvider } from "./contexts/AuthContext";
+import { AuthProvider, useAuth } from "./contexts/AuthContext";
 import { useGlobalListeners } from "./hooks/useGlobalListeners";
 import { useThemes } from "./hooks/useThemes";
 import { Navigation } from "./lib/navigation";
 
+SplashScreen.preventAutoHideAsync();
+
 function App() {
 	useGlobalListeners();
+
+	return (
+		<AuthProvider>
+			<AppContent />
+		</AuthProvider>
+	);
+}
+
+function AppContent() {
+	const { isLoading } = useAuth();
 	const { navigationTheme, paperTheme } = useThemes();
+
+	if (isLoading) return null;
 
 	return (
 		<PaperProvider theme={paperTheme}>
-			<AuthProvider>
-				<Navigation theme={navigationTheme} />
-			</AuthProvider>
+			<Navigation theme={navigationTheme} onReady={SplashScreen.hideAsync} />
 		</PaperProvider>
 	);
 }
--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -11,6 +11,7 @@ import {
 
 type AuthContext = {
 	session: Session | null;
+	isLoading: boolean;
 };
 
 const AuthContext = createContext<AuthContext | undefined>(undefined);
@@ -21,6 +22,7 @@ let currentSession: Session | null = null;
 export function AuthProvider(props: PropsWithChildren<object>) {
 	const { children } = props;
 	const [session, setSession] = useState<Session | null>(null);
+	const [isLoading, setIsLoading] = useState(true);
 
 	const updateSession = useCallback((session: Session | null) => {
 		setSession(session);
@@ -34,19 +36,27 @@ export function AuthProvider(props: PropsWithChildren<object>) {
 	useEffect(() => {
 		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
 			updateSession(newSession);
+			setIsLoading(false);
 		});
 
 		return data.subscription.unsubscribe;
 	}, [updateSession]);
 
 	async function checkSession() {
-		const { data } = await supabase.auth.getSession();
+		setIsLoading(true);
+		try {
+			const { data } = await supabase.auth.getSession();
 
-		updateSession(data.session);
+			updateSession(data.session);
+		} finally {
+			setIsLoading(false);
+		}
 	}
 
 	return (
-		<AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>
+		<AuthContext.Provider value={{ session, isLoading }}>
+			{children}
+		</AuthContext.Provider>
 	);
 }
 
