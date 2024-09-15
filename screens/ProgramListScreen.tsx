0dfd03f360ba302ea3cf09a353de128b6504901e
SMhd <smhd3000@gmail.com>
2024-09-15 14:52:56 +0330

    feat: redesign ProgramListScreen

--- screens/ProgramListScreen.tsx
+++ screens/ProgramListScreen.tsx
@@ -6,7 +6,7 @@ import {
 } from "@react-navigation/native";
 import { format } from "date-fns";
 import { FlatList, RefreshControl, View } from "react-native";
-import { Appbar, Card, Paragraph, Surface, Title } from "react-native-paper";
+import { Appbar, Card, Icon, Surface, Text } from "react-native-paper";
 import { createStyleSheet, useStyles } from "react-native-unistyles";
 
 type Props = StaticScreenProps<{
@@ -16,7 +16,7 @@ type Props = StaticScreenProps<{
 
 function ProgramListScreen(props: Props) {
 	const { conferenceId, title } = props.route.params;
-	const { styles } = useStyles(stylesheet);
+	const { styles, theme } = useStyles(stylesheet);
 
 	const navigation = useNavigation();
 	const { data, isFetching, refetch } = useProgramListQuery({ conferenceId });
@@ -24,21 +24,53 @@ function ProgramListScreen(props: Props) {
 	const renderProgramItem = ({ item }: { item: Tables<"Program"> }) => {
 		const startDate = new Date(item.start_date * 1000);
 		const endDate = new Date(item.end_date * 1000);
-		const pattern = "PPP hh:mm aaa";
+		const pattern = "hh:mmaaa";
 		const formattedStartDate = format(startDate, pattern);
 		const formattedEndDate = format(endDate, pattern);
 
 		return (
-			<Card style={styles.card}>
-				<Card.Content>
-					<Title>{item.name}</Title>
-					<Paragraph>{item.description}</Paragraph>
-					<Paragraph>Location: {item.location}</Paragraph>
-					<View style={styles.dateContainer}>
-						<Paragraph>From: {formattedStartDate}</Paragraph>
-						<Paragraph>To: {formattedEndDate}</Paragraph>
+			<Card style={styles.card} contentStyle={styles.cardContent}>
+				<View style={styles.timeline}>
+					<View style={styles.dot} />
+					<View style={styles.line} />
+					<View style={styles.dot} />
+				</View>
+
+				<View style={styles.details}>
+					<Text variant="titleMedium" style={styles.mainText}>
+						{item.name}
+					</Text>
+
+					{item.description && (
+						<Text variant="bodyMedium" style={styles.mainText}>
+							{item.description}
+						</Text>
+					)}
+
+					{item.location && (
+						<View style={styles.metaContainer}>
+							<Icon
+								source="map-marker-outline"
+								size={16}
+								color={theme.colors.primary}
+							/>
+							<Text variant="bodySmall" style={styles.metaText}>
+								{item.location}
+							</Text>
+						</View>
+					)}
+
+					<View style={styles.metaContainer}>
+						<Icon
+							source="clock-outline"
+							size={16}
+							color={theme.colors.primary}
+						/>
+						<Text variant="bodySmall" style={styles.metaText}>
+							{formattedStartDate} - {formattedEndDate}
+						</Text>
 					</View>
-				</Card.Content>
+				</View>
 			</Card>
 		);
 	};
@@ -74,10 +106,43 @@ const stylesheet = createStyleSheet(({ colors }, { insets }) => ({
 		padding: 16,
 	},
 	card: {
+		marginVertical: 8,
 		backgroundColor: colors.surface,
-		marginBottom: 16,
 	},
-	dateContainer: {
-		marginTop: 8,
+	cardContent: {
+		flexDirection: "row",
+		padding: 16,
+		columnGap: 16,
+	},
+	timeline: {
+		justifyContent: "center",
+		alignItems: "center",
+		rowGap: 4,
+	},
+	dot: {
+		width: 8,
+		height: 8,
+		borderRadius: 4,
+		backgroundColor: colors.secondary,
+	},
+	line: {
+		flex: 1,
+		width: 2,
+		backgroundColor: colors.secondary,
+	},
+	details: {
+		rowGap: 8,
+	},
+	mainText: {
+		color: colors.onSurface,
+	},
+	metaContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		flex: 1,
+		gap: 8,
+	},
+	metaText: {
+		color: colors.onSurface,
 	},
 }));
